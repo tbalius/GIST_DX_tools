0004ef7746f2d13e99fafc67db71a0a3d8791237
Trent E. Balius (UCSF) <tbalius@gimel.cluster.ucsf.bkslab.org>
2017-04-17 11:19:21 -0700

    modifed dx-gist_score_gist_sph.py and add clusterEnergies_from_dx-sph.py dx-gist_make_centers_of_intensity.py

---
+++ src/clusterEnergies_from_dx-sph.py
@@ -0,0 +1,229 @@
+
+import sys, os, math
+import mol2
+import sph_lib
+import pdb_lib
+import dx_gist_lib as dxlib 
+
+# this is written by Trent Balius in the Shoichet Lab
+# written in march, 2016
+# changed names, MF -- March 2017
+
+def distance(v1,v2):
+    if (len(v1)!=len(v2)):
+       print "error" 
+       exit()
+    dist = 0.0
+    for i in range(len(v1)):
+        dist = dist + (v1[i]-v2[i])**2.0
+    dist = math.sqrt(dist)
+    return dist
+
+def cal_grid(values,gridscale,xn,yn,zn,origin):
+   # here the vector of values is transformed to a multidemitional array (grid)
+    grid_old = []
+    count = 0
+    for i in range(xn):
+        ydim = []
+        for j in range(yn):
+            zdem = []
+            for k in range(zn):
+                zdem.append(values[count])
+                count = count + 1
+            ydim.append(zdem)
+        grid_old.append(ydim)
+    return grid_old
+
+
+######################################################################
+# this function calculates gist score. 
+# also calculates peratom controbutions distributing overlap 
+# atoms equaly among all atoms contating the point
+# also calculates volume displaced by molcule.
+######################################################################
+
+def calc_score(fileprefix,values,gridscale,xn,yn,zn,origin,sphs,fileh,return_vals):
+# file - log file
+# values gist values 
+# gridscale - the grid spacing
+
+
+    # loop over each sphere. 
+
+    dict_gridpoint = {} # for each grid point have a list of atoms it is in
+
+    
+    sum_per_atom = []
+    for atom_i,atom in enumerate(sphs):
+        sum_per_atom.append(0.0)
+
+        radius = atom.radius
+        #print atom.type, atom.X, atom.Y, atom.Z, atom.type, dt[atom_i], radius
+
+        grid_i = round((atom.X - origin[0] ) / gridscale)
+        grid_j = round((atom.Y - origin[1] ) / gridscale)
+        grid_k = round((atom.Z - origin[2] ) / gridscale)
+
+        if (grid_i<0.0 or grid_j<0.0 or grid_k<0.0):
+           print "ERROR. . . "
+           exit()
+
+        radius_gridpoint = math.ceil(radius / gridscale) + 1
+        #print radius, gridscale, radius_gridpoint
+        #print  grid_i,grid_j, grid_k
+
+        start_i = max([(grid_i - radius_gridpoint),0.0])
+        stop_i  = min([(grid_i + radius_gridpoint),xn])
+        start_j = max([(grid_j - radius_gridpoint),0.0])
+        stop_j  = min([(grid_j + radius_gridpoint),xn])
+        start_k = max([(grid_k - radius_gridpoint),0.0])
+        stop_k  = min([(grid_k + radius_gridpoint),xn])
+
+        for i in range(int(start_i),int(stop_i)): # looping over the cube about the center of the atom 
+            x = (i * gridscale) + origin[0] 
+            for j in range(int(start_j),int(stop_j)):
+                y = (j * gridscale) + origin[1] 
+                for k in range(int(start_k),int(stop_k)):
+                    z = (k * gridscale) + origin[2] 
+                    dist = distance([x,y,z],[atom.X,atom.Y,atom.Z])
+                    if (dist <= radius):
+                        #count = i*((xn)**2)+j*(xn) + k
+                        count = i*(yn*zn)+j*(zn) + k
+                        if not (count in dict_gridpoint):
+                           dict_gridpoint[count] = [atom_i]
+                        else:
+                           dict_gridpoint[count].append(atom_i)
+    sum_val = 0
+    sum_val_positive = 0
+    sum_val_negative = 0
+    voxel_vol =  gridscale**3.0
+    # loop over all displaced grid points.
+    for key in dict_gridpoint.keys():
+        # sum up total value as well as positive, and negative contrabutions.   
+        sum_val = sum_val + values[key]
+        if values[key] > 0:
+           sum_val_positive = sum_val_positive + values[key]
+        else:
+           sum_val_negative = sum_val_negative + values[key]
+
+        # also calculate the per-atom gist decomposition. 
+        # if a point is shared by multiple atoms, divied the value by the 
+        # number of atoms that share that point and asign that fractional 
+        # value to each atom
+        N = len(dict_gridpoint[key])
+        for atom_i in dict_gridpoint[key]:
+            #print atom_i
+            sum_per_atom[atom_i] = sum_per_atom[atom_i] + (values[key]/N)
+
+    molN = len(dict_gridpoint.keys())
+    boxN = len(values)
+    boxV = xn*gridscale*yn*gridscale*zn*gridscale
+
+    molV = float(molN)/float(boxN)*boxV
+
+    print "gist_val:", sum_val*voxel_vol
+    print "gist_val_positive:", sum_val_positive*voxel_vol
+    print "gist_val_negative:", sum_val_negative*voxel_vol
+    print "molN=",molN,"  boxN=",boxN,"  boxV=",boxV
+    print "molV=",molV
+    
+    fileh.write('%s,%f\n'%("gist_val", sum_val*voxel_vol))
+    fileh.write('%s,%f\n'%("gist_val_positive", sum_val_positive*voxel_vol))
+    fileh.write('%s,%f\n'%("gist_val_negative", sum_val_negative*voxel_vol))
+    fileh.write('%s,%f\n'%("molN",molN))
+    fileh.write('%s,%f\n'%("boxN",boxN))
+    fileh.write('%s,%f\n'%("boxV",boxV))
+    fileh.write('%s,%f\n'%("molV",molV))
+
+    atom_energies = []
+    for i,atom in enumerate(sphs):
+        print i,":",sum_per_atom[i]*voxel_vol
+        fileh.write('atom%d,%f\n'%(i+1,sum_per_atom[i]*voxel_vol))
+        atom_energies.append(sum_per_atom[i]*voxel_vol)
+
+    new_values = []
+    # make a new grid with only the voxels in the ligand are non-zerro
+
+    if (return_vals):
+        new_values = []
+        for i,val in enumerate(values):
+            #print i 
+            if i in dict_gridpoint:
+            #if i in dict_gridpoint.keys():
+               new_values.append(val)
+            else:
+               new_values.append(0.0)
+
+    return new_values, atom_energies 
+
+def extract_fileprefix_from_path(filepath):
+
+   filename = filepath.split('/')[-1] # split dx filename on "/"
+   splitfilename   = filename.split('.') 
+   if (len(splitfilename)>2): 
+       print "In path "+filepath+" filename has too many periods. . ."
+       exit()
+   return splitfilename[0]
+
+def convert_sph_to_pdb_and_write(sphs,energies,pdbfilename):
+
+   molname = "CLU"
+   chainid = "A"   
+   resname = "CLU"
+   resnum  = 1
+   atomname = "O"
+
+   i = 0
+   atoms = []
+   for sph in sphs:
+      atomnum  = sph.atomnum
+      X = sph.X
+      Y = sph.Y
+      Z = sph.Z
+      bfact = energies[i]
+      boolhet = False
+      atom = pdb_lib.PDB_atom_info(molname, chainid, resname, resnum, atomname, atomnum, X, Y, Z, bfact, boolhet) 
+      atoms.append(atom)
+      i=i+1
+   pdb_lib.output_pdb(atoms,pdbfilename)
+
+
+def main():
+
+
+   if len(sys.argv) != 3: # if no input
+       print "ERORR:"
+       print "syntex: dx-gist_rescore.py dx-file sphere_file"
+       print "dx-input-file input file in dx formate produed by gist, may be disities or energies"
+       print "sphere file that defines the binding site (low dielectric sphere recomended) "
+       return
+ 
+
+   infiledx      = sys.argv[1]
+   infilesph     = sys.argv[2]
+
+   prefixdx = extract_fileprefix_from_path(infiledx)
+   prefixsph = extract_fileprefix_from_path(infilesph)
+
+   #outfile       = "site_energetics_dx-"+prefixdx+"sph-"+prefixsph
+   outfile        = "clustEnergy"+prefixsph
+
+   print infiledx
+   print infilesph
+
+   xn,yn,zn,dx,dy,dz,origin,values = dxlib.read_in_dx_file(infiledx)
+
+   gridscale = dx # assumes that they are all the same spaceing
+   
+   print gridscale ,xn,yn,zn,dx,dy,dz,origin   
+   sphs  = sph_lib.read_sph(infilesph,'A','A')
+
+   file1 = open(outfile+'gist_values.txt','w')
+
+   new_values, atom_energies  = calc_score(outfile+'.txt',values,gridscale,xn,yn,zn,origin, sphs, file1, True)
+   dxlib.write_out_dx_file(outfile+'.dx',xn,yn,zn,dx,dy,dz,origin,new_values)
+   convert_sph_to_pdb_and_write(sphs,atom_energies,outfile+'.pdb')
+   file1.close()
+main()
+
+
---
+++ src/dx-gist_make_centers_of_intensity.py
@@ -0,0 +1,247 @@
+
+import sys, os, math
+import dx_gist_lib as dxlib
+
+# this is written by Trent Balius in the Shoichet Lab
+# written in june, 2015
+#
+# changed names, MF -- March 2017
+#
+# here we read in dx grid of some values (energies, denisties) and write out the center of intenities.
+# frist apply a cut of.
+# then clusters are determined of conneted grid points 
+# (a point is in a cluster if that point is next to at least one other point in that cluster)
+# the weighted center of each cluster is then calculated. 
+
+class Point:
+    def __init__(self,X,Y,Z,I,J,K,V,C):
+        self.X = float(X) # coordenates
+        self.Y = float(Y)
+        self.Z = float(Z)
+        self.I = int(I) # grid index
+        self.J = int(J)
+        self.K = int(K)
+        self.V = float(V) # value
+        self.C = int(C) # cluster
+
+
+######################################################################
+# this function takes as input a list of points and
+# returns a dictionary of clusters.
+# the lookup value is the point number, the definition is the cluster number
+######################################################################
+def cluster_points(points,dist):
+
+    bonds = []
+    bonded_p = {} # rember which are bonded. 
+    N = len(points)
+    # loop over list of points look at which are bonded. 
+    # if 
+    for i in range(N):
+        ind_i1 = points[i].I 
+        ind_j1 = points[i].J 
+        ind_k1 = points[i].K 
+        for j in range(i+1,N):
+            ind_i2 = points[j].I 
+            ind_j2 = points[j].J 
+            ind_k2 = points[j].K 
+            if (math.fabs(ind_i2-ind_i1) + math.fabs(ind_j2-ind_j1) + math.fabs(ind_k2-ind_k1)) == 0:
+                print "Error. same point. duplicate found."
+                exit()
+            if (math.fabs(ind_i2-ind_i1) + math.fabs(ind_j2-ind_j1) + math.fabs(ind_k2-ind_k1)) <= dist: # if 0, they are the same, if grater than dist (one or two, must be an integer) they are not connected.  
+               bond = [i, j]
+               bonds.append(bond)
+               bonded_p[i] = 1
+               bonded_p[j] = 1
+               print bond
+    
+    clusters = {}
+    count = 0
+    # 
+    for bond in bonds:
+        if bond[0] in clusters: # if the starting point is in a cluster then put the ending point in that same cluster
+           clusters[bond[1]] = clusters[bond[0]]
+        elif bond[1] in clusters: # if the ending point is in a cluster then put the starting point in that same cluster
+           clusters[bond[0]] = clusters[bond[1]]
+        else: # nether point is in a cluster
+           clusters[bond[0]] = count 
+           clusters[bond[1]] = count 
+           count = count +1
+    for i in range(N):
+        if not i in bonded_p:
+           # then singlton.
+           clusters[i] = count
+           count = count+1
+
+
+    print clusters           
+    for key in clusters.keys():
+        print key, clusters[key]
+
+    return clusters, count
+        
+######################################################################
+######################################################################
+
+def write_pdb(points,fileh):
+
+    count = 0
+    for point in points:
+        fileh.write("ATOM  %5d  %-3s %3s %1s %3d %11.3f %7.3f %7.3f %5.2f\n" % (count, "CA", "CLU", "A", point.C, point.X, point.Y, point.Z, point.V/100.0 ))
+        count = count + 1
+       
+######################################################################
+# this function makes the cluster centers. 
+# first find all all connected grid point after applying a threshold
+# then calculate the weighted mean. 
+# then print the  
+######################################################################
+
+def make_centers(fileprefix,values,gridscale,xn,yn,zn,origin,sign,threshold,dist):
+# file - log file
+# values gist values 
+# gridscale - the grid spacing
+# number -- number of grid spacings to combine
+# sign -- direction to cluster
+
+
+    fileh = open(fileprefix+'_clusters.pdb','w')
+    fileh2 = open(fileprefix+'_points.pdb','w')
+
+    new_values = []
+    countx = 0
+    county = 0
+    countz = 0
+    #yflag = False
+    #zflag = False
+    #gridscale_new = gridscale*number
+
+    ## here is how to calculate the new orgin: 
+    ## x_new = x + gridscale / number * (1 + 2 + ... + number) 
+    #sum = 0
+    #for i in range (1,number): 
+    #     sum = sum + i 
+    #print "num=",number, 'sum=',sum
+    #
+    #ox_n =  origin[0] + gridscale / number * sum
+    #oy_n =  origin[1] + gridscale / number * sum
+    #oz_n =  origin[2] + gridscale / number * sum
+    #origin_new = [ox_n,oy_n,oz_n]
+#
+    #print origin_new
+
+    # here the vector of values is transformed to a multidemitional array (grid)
+    grid_old = [] 
+    count = 0
+    for i in range(xn):
+        ydim = [] 
+        for j in range(yn):
+            zdem = []
+            for k in range(zn):
+                zdem.append(values[count])
+                count = count + 1
+            ydim.append(zdem)
+        grid_old.append(ydim)
+
+    points = []
+
+    # here we idenify grid points to be clustered
+    for i in range(xn):
+        x = (i * gridscale) + origin[0] 
+        for j in range(yn):
+            y = (j * gridscale) + origin[1] 
+            for k in range(zn):
+                z = (k * gridscale) + origin[2] 
+                if (sign*grid_old[i][j][k] > threshold): # we only use the sign in the comparison with Threshold. 
+                    print x,y,z, grid_old[i][j][k]
+                    point = Point(x,y,z,i,j,k,grid_old[i][j][k],-1)
+                    points.append(point)
+
+    # here we cluster the points.  
+    clusters, count = cluster_points(points,dist) 
+
+    # put which cluster the points belong to in date structure.  
+    for key in clusters.keys():
+         i = key
+         c = clusters[key]
+         points[i].C = c
+    write_pdb(points,fileh2)
+
+    print "there are "+ str(count)+" clusters"
+
+    # initalize a weight mean cluster point
+    weight_mean_points = []
+    
+    num_cluster_points = []
+    for c in range(count): # loop over the number of clusters
+          weight_mean_points.append(Point(0.0,0.0,0.0,0,0,0,0.0,0))
+          num_cluster_points.append(0)
+
+    # calculate weighted mean of the cluters.
+    for key in clusters.keys():
+         i = key
+         c = clusters[key]
+         #print key, clusters[key]
+         #weight_mean_points[c].X = weight_mean_points[c].X + points[i].X
+         #weight_mean_points[c].Y = weight_mean_points[c].Y + points[i].Y
+         #weight_mean_points[c].Z = weight_mean_points[c].Z + points[i].Z
+         weight_mean_points[c].X = weight_mean_points[c].X + points[i].V * points[i].X
+         weight_mean_points[c].Y = weight_mean_points[c].Y + points[i].V * points[i].Y
+         weight_mean_points[c].Z = weight_mean_points[c].Z + points[i].V * points[i].Z
+         weight_mean_points[c].V = weight_mean_points[c].V + points[i].V
+         weight_mean_points[c].C = c
+         num_cluster_points[c] = num_cluster_points[c] + 1  
+    for c in range(count): # loop over number of clusters
+         #weight_mean_points[c].X = weight_mean_points[c].X / num_cluster_points[c]
+         #weight_mean_points[c].Y = weight_mean_points[c].Y / num_cluster_points[c]
+         #weight_mean_points[c].Z = weight_mean_points[c].Z / num_cluster_points[c]
+         weight_mean_points[c].X = weight_mean_points[c].X / weight_mean_points[c].V 
+         weight_mean_points[c].Y = weight_mean_points[c].Y / weight_mean_points[c].V 
+         weight_mean_points[c].Z = weight_mean_points[c].Z / weight_mean_points[c].V 
+         print c, weight_mean_points[c].X, weight_mean_points[c].Y, weight_mean_points[c].Z, weight_mean_points[c].V
+
+
+    write_pdb(weight_mean_points,fileh)
+
+    fileh.close()
+    fileh2.close()
+    return 
+
+
+
+
+
+def main():
+
+   if len(sys.argv) != 6: # if no input
+       print "ERORR:"
+       print "syntex: dx-gist_make_centers.py dx-input-file sign cutoff distint pdb-output-file"
+       print "sign can be 1 or -1: if one should always be used for densities,"
+       print "                     for energies, weight of one means pdb contains cordenates where "
+       print "                     waters do not want to be(anti-waters)."
+       print "                     for energies, weight of negative one means pdb contains cordenates "
+       print "                     where waters do want to be (negative energies)."
+       print "cutoff can only be a positive floating point number. "
+       print "                     this point is used to deterint clusters of points" 
+       print "distint howmeny grid points away considered conected. should be 1 (0.5 angstroms) or 2(1.0 A), 3 (1.5 A)"
+       print "dx-input-file input file in dx formate produed by gist, may be disities or energies"
+       print "pdb-output-file is an output file in pdb formate contains the weighted mean poition of clusters"
+       return
+ 
+
+   infile1    = sys.argv[1]
+   sign       = float(sys.argv[2])
+   threshold  = float(sys.argv[3])
+   dist       = int(sys.argv[4])
+   outfile    = sys.argv[5]
+
+   xn,yn,zn,dx,dy,dz,origin,values = dxlib.read_in_dx_file(infile1)
+
+   gridscale = dx # assumes that they are all the same spaceing
+
+   make_centers(outfile,values,gridscale,xn,yn,zn,origin, sign, threshold, dist)
+
+
+main()
+
+
